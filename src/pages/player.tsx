0124c9ef67f91339ee71f6e6489164e2d32d87af
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-09-13 18:39:16 -0300

    add reducer and slices

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -4,8 +4,11 @@ import { Header } from "../components/Header";
 
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
+import { useAppSelector } from "../store";
 
 export function Player() {
+  const modules = useAppSelector((state) => state.player.course.modules);
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
@@ -25,21 +28,16 @@ export function Player() {
           </div>
 
           <aside className="divide-w-2 divide-zinc-900 w-80 absolute top-0 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            <Module
-              moduleIndex={1}
-              title="Desvendando o Redux"
-              amountOfLessons={3}
-            />
-            <Module
-              moduleIndex={2}
-              title="Desvendando o Redux"
-              amountOfLessons={3}
-            />
-            <Module
-              moduleIndex={3}
-              title="Desvendando o Redux"
-              amountOfLessons={3}
-            />
+            {modules.map((module, index) => {
+              return (
+                <Module
+                  key={module.id}
+                  moduleIndex={index}
+                  title={module.title}
+                  amountOfLessons={module.lessons.length}
+                />
+              );
+            })}
           </aside>
         </main>
       </div>
